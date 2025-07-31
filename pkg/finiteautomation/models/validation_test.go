3193080ba3a70f0a307b93bf8f45a7766751f968
hoanglam1990 <pnhlam1990@gmail.com>
2025-07-31 10:08:16 -0300

    Update validation_test.go

--- pkg/finiteautomation/models/validation_test.go
+++ pkg/finiteautomation/models/validation_test.go
@@ -37,6 +37,8 @@ func TestAreAcceptionStatesValid_Invalid(t *testing.T) {
 	}
 }
 
+// TestIsInitialStateValid_Valid tests that IsInitialStateValid succeeds
+// when the provided initial state is included in the finite state set.
 func TestIsInitialStateValid_Valid(t *testing.T) {
 	state1, state2 := State{}, State{}
 	state1.Initialize("0", map[string]*State{})
@@ -51,6 +53,8 @@ func TestIsInitialStateValid_Valid(t *testing.T) {
 	}
 }
 
+// TestIsInitialStateValid_Invalid tests an invalid initial state 
+// that is not part of the finite state set.
 func TestIsInitialStateValid_Invalid(t *testing.T) {
 	state1, state2, state3 := State{}, State{}, State{}
 	state1.Initialize("0", map[string]*State{})
@@ -59,13 +63,15 @@ func TestIsInitialStateValid_Invalid(t *testing.T) {
 
 	finiteStates := map[*State]*State{&state1: &state1, &state2: &state2}
 
-	err := IsInitialStateValid(finiteStates, &state3)
+	err := IsInitialStateValid(finiteStates, &state3) // <-- state3 not in the list
 
 	if err == nil {
 		t.Errorf("Expected error for invalid initial state, got nil")
 	}
 }
 
+// TestAreTransitionFunctionsValid_Valid tests that AreTransitionFunctionsValid correctly
+// validates a set of transition functions
 func TestAreTransitionFunctionsValid_Valid(t *testing.T) {
 	state1, state2 := State{}, State{}
 	state1.Initialize("0", map[string]*State{})
@@ -87,11 +93,13 @@ func TestAreTransitionFunctionsValid_Valid(t *testing.T) {
 	}
 }
 
+// TestAreTransitionFunctionsValid_InvalidCurrentState tests that AreTransitionFunctionsValid
+// correctly identifies a transition function with an invalid current state.
 func TestAreTransitionFunctionsValid_InvalidCurrentState(t *testing.T) {
 	state1, state2, state3 := State{}, State{}, State{}
 	state1.Initialize("0", map[string]*State{})
 	state2.Initialize("1", map[string]*State{})
-	state3.Initialize("2", map[string]*State{})
+	state3.Initialize("2", map[string]*State{}) // <-- invalid current state
 	finiteStates := map[*State]*State{&state1: &state1, &state2: &state2}
 
 	inputs := map[string]bool{"0": true, "1": true}
@@ -99,7 +107,7 @@ func TestAreTransitionFunctionsValid_InvalidCurrentState(t *testing.T) {
 	tf1 := TransitionFunction{}
 	tf1.Initialize(&state1, "0", &state1)
 	tf2 := TransitionFunction{}
-	tf2.Initialize(&state3, "1", &state1)
+	tf2.Initialize(&state3, "1", &state1) // <-- transition with invalid starting state (state3 not in the list)
 	transitionFunctions := []TransitionFunction{tf1, tf2}
 
 	err := AreTransitionFunctionsValid(finiteStates, inputs, transitionFunctions)
@@ -109,11 +117,13 @@ func TestAreTransitionFunctionsValid_InvalidCurrentState(t *testing.T) {
 	}
 }
 
+// TestAreTransitionFunctionsValid_InvalidCurrentState tests that AreTransitionFunctionsValid
+// correctly identifies a transition function with an invalid transition state.
 func TestAreTransitionFunctionsValid_InvalidTransitionState(t *testing.T) {
 	state1, state2, state3 := State{}, State{}, State{}
 	state1.Initialize("0", map[string]*State{})
 	state2.Initialize("1", map[string]*State{})
-	state3.Initialize("2", map[string]*State{})
+	state3.Initialize("2", map[string]*State{}) // <-- invalid transition state
 	finiteStates := map[*State]*State{&state1: &state1, &state2: &state2}
 
 	inputs := map[string]bool{"0": true, "1": true}
@@ -121,7 +131,7 @@ func TestAreTransitionFunctionsValid_InvalidTransitionState(t *testing.T) {
 	tf1 := TransitionFunction{}
 	tf1.Initialize(&state1, "0", &state1)
 	tf2 := TransitionFunction{}
-	tf2.Initialize(&state2, "1", &state3)
+	tf2.Initialize(&state2, "1", &state3) // <-- transition with invalid transition state (state3 not in the list)
 	transitionFunctions := []TransitionFunction{tf1, tf2}
 
 	err := AreTransitionFunctionsValid(finiteStates, inputs, transitionFunctions)
@@ -131,18 +141,20 @@ func TestAreTransitionFunctionsValid_InvalidTransitionState(t *testing.T) {
 	}
 }
 
+// TestAreTransitionFunctionsValid_InvalidCurrentState tests that AreTransitionFunctionsValid
+// correctly identifies an input not in the list of finite inputs.
 func TestAreTransitionFunctionsValid_InvalidInputs(t *testing.T) {
 	state1, state2 := State{}, State{}
 	state1.Initialize("0", map[string]*State{})
 	state2.Initialize("1", map[string]*State{})
 	finiteStates := map[*State]*State{&state1: &state1, &state2: &state2}
 
-	inputs := map[string]bool{"0": true, "2": true}
+	inputs := map[string]bool{"0": true, "2": true} // <-- accepting inputs 0 & 2
 
 	tf1 := TransitionFunction{}
 	tf1.Initialize(&state1, "0", &state1)
 	tf2 := TransitionFunction{}
-	tf2.Initialize(&state2, "1", &state1)
+	tf2.Initialize(&state2, "1", &state1) // <-- invalid input 1
 	transitionFunctions := []TransitionFunction{tf1, tf2}
 
 	err := AreTransitionFunctionsValid(finiteStates, inputs, transitionFunctions)
